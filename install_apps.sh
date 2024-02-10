5c06acbe2a3f66d280a445c822164ca0efdba668
Steve Pentland <stevepentland@gmail.com>
2024-02-10 17:14:05 -0500

    add more base apps

--- install_apps.sh
+++ install_apps.sh
@@ -3,4 +3,15 @@
 # Install additional dependencies for my dotfiles
 # Usage: ./install_deps.sh
 
-sudo pacman -S neovim alacritty fish tmux stow
+sudo pacman -S neovim alacritty fish tmux stow \
+	ripgrep github-cli fd python-pip python-pipx \
+	lazygit python-pynvim thunderbird exa fzf flatpak \
+	libssh unixodbc libxslt ncurses chromium micro \
+	gnome-keyring scrcpy discord bitwarden libsecret \
+	firefox curl
+
+sudo pacman -S --needed base-devel perl-authen-sasl perl-io-socker-ssl
+
+yay -S appimagelauncher
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
