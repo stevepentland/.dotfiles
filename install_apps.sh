c46302e146588cd1e2efb2153ead8e0c3767333f
Steve Pentland <stevepentland@gmail.com>
2024-02-11 16:27:11 -0500

    try again to make fisher more portable

--- install_apps.sh
+++ install_apps.sh
@@ -8,9 +8,9 @@ sudo pacman -S neovim alacritty fish tmux stow \
 	lazygit python-pynvim thunderbird exa fzf flatpak \
 	libssh unixodbc libxslt ncurses chromium micro \
 	gnome-keyring scrcpy discord bitwarden libsecret \
-	firefox curl
+	firefox curl nerd-fonts
 
-sudo pacman -S --needed base-devel perl-authen-sasl perl-io-socker-ssl
+sudo pacman -S --needed base-devel perl-authen-sasl perl-io-socket-ssl
 
 yay -S appimagelauncher
 
